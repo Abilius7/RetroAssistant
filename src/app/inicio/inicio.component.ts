7394707205bed7d10382ab386adc44a9a3f05ca1
Abilius7 <abel.fercab@gmail.com>
2022-02-23 16:01:36 +0000

    Volvemos a cambiar algun componente responsibe a mayores para presentarlo

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -23,38 +23,48 @@ export class InicioComponent implements OnInit {
   visibilizarDescripcion(e: any) {
 
     let final=0;
-    if (screen.width <600){
+    if (screen.width <768){
       final=25
+      e.target.parentNode.style.display = "inline-block";
+      if (e.target.parentNode.querySelector("figcaption").style.visibility == "hidden") {
+        e.target.parentNode.querySelector("figcaption").style.visibility = "visible";
+        e.target.parentNode.querySelector("figcaption").style.display = "inline-block";
+        e.target.parentNode.querySelector("figcaption").style.marginLeft="5%";
+      }else{
+        e.target.parentNode.querySelector("figcaption").style.visibility = "hidden";
+        e.target.parentNode.querySelector("figcaption").style.display = "none"
+      }
     }else{
       final=45;
+      if (e.target.parentNode.querySelector("figcaption").style.visibility == "hidden") {
+        let contador = final;
+        e.target.parentNode.querySelector("figcaption").style.visibility = "hidden";
+        e.target.parentNode.querySelector("figcaption").style.display = "none";
+        let animacionEntrada = setInterval(() => {
+          e.target.parentNode.querySelector("img").style.left = contador + "%";
+          contador--;
+          if (contador == 0) {
+            clearInterval(animacionEntrada);
+            e.target.parentNode.querySelector("figcaption").style.visibility = "visible";
+            e.target.parentNode.querySelector("figcaption").style.display = "block";
+          }
+        }, 20)
+      }else{
+        e.target.parentNode.querySelector("figcaption").style.visibility = "hidden"
+        e.target.parentNode.querySelector("figcaption").style.display = "none";
+        let contador = e.target.parentNode.querySelector("img").style.left.split("%")[0];
+        let animacionSalida = setInterval(() => {
+          e.target.parentNode.querySelector("img").style.left = contador + "%";
+          contador++;
+          if (contador == final) {
+            clearInterval(animacionSalida);
+            ;
+          }
+        }, 10)
+      }
     }
 
-    if (e.target.parentNode.querySelector("figcaption").style.visibility == "hidden") {
-      let contador = final;
-      e.target.parentNode.querySelector("figcaption").style.visibility = "hidden";
-      e.target.parentNode.querySelector("figcaption").style.display = "none";
-      let animacionEntrada = setInterval(() => {
-        e.target.parentNode.querySelector("img").style.left = contador + "%";
-        contador--;
-        if (contador == 0) {
-          clearInterval(animacionEntrada);
-          e.target.parentNode.querySelector("figcaption").style.visibility = "visible";
-          e.target.parentNode.querySelector("figcaption").style.display = "block";
-        }
-      }, 20)
-    }else{
-      e.target.parentNode.querySelector("figcaption").style.visibility = "hidden"
-      e.target.parentNode.querySelector("figcaption").style.display = "none";
-      let contador = e.target.parentNode.querySelector("img").style.left.split("%")[0];
-      let animacionSalida = setInterval(() => {
-        e.target.parentNode.querySelector("img").style.left = contador + "%";
-        contador++;
-        if (contador == final) {
-          clearInterval(animacionSalida);
-          ;
-        }
-      }, 10)
-    }
+    
 
   }
 
