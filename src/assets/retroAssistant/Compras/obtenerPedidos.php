f62755868cc7ad6afe1cb8d5025969514b6b4755
Abilius7 <abel.fercab@gmail.com>
2022-02-23 16:10:36 +0000

    Actualizacion de los archivos de servidor y annado instrucciones

--- src/assets/retroAssistant/Compras/obtenerPedidos.php
+++ src/assets/retroAssistant/Compras/obtenerPedidos.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
     $conexion = new mysqli("localhost","Usuario","2DAW","RetroAssistant");
     if ($conexion->connect_errno){
         print $conexion->connect_errno;
@@ -6,7 +11,7 @@
         $pedidos = [];
         $objDatos = json_decode(file_get_contents("php://input"));
         $idUsuario=$objDatos->idUsuario;
-        $queryPedidos=$conexion->query("SELECT * FROM pedido WHERE idUsuario =$idUsuario");
+        $queryPedidos=$conexion->query("SELECT * FROM pedido WHERE idUsuario =$idUsuario ORDER BY Fecha DESC");
 
         while ($pedido = $queryPedidos->fetch_array(MYSQLI_ASSOC)){
             $idPedido = $pedido['idPedido'];
